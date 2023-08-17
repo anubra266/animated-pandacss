6574b1ff3d562c81cdeab7b659b4a22332fd9467
anubra266 <anubra266@gmail.com>
2023-08-17 22:13:35 +0100

    chore: add demo site

---
+++ site/src/App.tsx
@@ -0,0 +1,11 @@
+import { css } from '../styled-system/css'
+
+function App() {
+  return (
+    <>
+      <div className={css({ color: 'red.400' })}>wow</div>
+    </>
+  )
+}
+
+export default App
